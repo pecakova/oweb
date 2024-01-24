eb31f5e057354428dcf1aa7d80efaf8082934594
Bojana <pecakovab@gmail.comm>
2024-01-24 18:03:21 +0100

    commit

---
+++ domashni/angular/Domashno5/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { DriverComponent } from './driver/driver.component';
+import { DRIVERS } from '../Angular (1)/db-data';
+import { Driver } from './module/klasa';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, DriverComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Domashno5';
+
+  maks = DRIVERS[1];
+  checo = DRIVERS[2];
+  lewis = DRIVERS[3];
+  vozaci = DRIVERS;
+
+  appKlik(suzi: Driver) {
+    console.log("Me klikna od app", suzi);
+  }
+}
